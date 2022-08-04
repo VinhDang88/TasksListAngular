ecf95709f0141253873dbd6ed5c7175aab03f670
VinhDang88 <vincedang720@gmail.com>
2022-08-04 19:24:58 -0400

    Had to commit to correct Folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDo';
+  ToDoLists: Todo[] = [
+  { 
+    task: "Pet Dog",
+    completed: true
+  },
+  {
+    task: "Pet Cat",
+    completed: true
+
+  },
+  {
+    task: "Eat Donut",
+    completed: false
+
+  }
+  
+
+  ];
+
+  addTasks(form:NgForm):void{
+    let newTask:Todo = {
+      task: form.form.value.task,
+      completed: false
+
+    }
+    this.ToDoLists.push(newTask)
+
+  }
+
+  completeTask(task:Todo):void{
+  task.completed = true;
+
+  }
+
 }
